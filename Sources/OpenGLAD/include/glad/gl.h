79f98f6187df85f2ffc9f767cd288d1e8f903311
Helbert Gomes <helbertgs@gmail.com>
2026-06-30 13:19:16 -0400

    chore: organize the C source and header layoutc

--- Sources/OpenGLAD/include/glad/gl.h
+++ Sources/OpenGLAD/include/glad/gl.h
@@ -1066,7 +1066,7 @@ typedef void (*GLADpostcallback)(void *ret, const char *name, GLADapiproc apipro
 #define GL_ZERO 0
 
 
-#include "khrplatform.h"
+#include "../khr/khrplatform.h"
 typedef unsigned int GLenum;
 typedef unsigned char GLboolean;
 typedef unsigned int GLbitfield;
